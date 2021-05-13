c3b3cb6b94e8dfbdb32c64b9b832310a1d7bf97f
Ethan <ethanhilljay@outlook.com>
2021-05-13 16:35:09 +0100

    Updated to use the Options component

--- frontend/components/ContactCard.vue
+++ frontend/components/ContactCard.vue
@@ -33,34 +33,21 @@
             </div>
           </div>
         </div>
-        <div class="flex justify-around flex-1 w-full mt-4 lg:mt-0">
-          <div
-            class="inline-flex transition-all bg-white rounded-md shadow hover:bg-yellow-500"
-          >
-            <button
-              class="px-6 py-2 font-bold text-gray-700 hover:text-white"
-              @click="onEdit($event, contact.id)"
-            >
-              Edit
-            </button>
-          </div>
-          <div
-            class="relative inline-flex transition-all bg-white rounded-md shadow hover:bg-red-500"
-          >
-            <button
-              class="px-6 py-2 font-bold text-gray-700 hover:text-white"
-              @click="onDelete($event, contact.id)"
-            >
-              Delete
-            </button>
-          </div>
-        </div>
+        <Options
+          :contact="contact"
+          :delete-contact="onDelete"
+          :edit-contact="onEdit"
+        />
       </div>
     </div>
   </div>
 </template>
 <script>
+import Options from './Items/Options'
 export default {
+  components: {
+    Options,
+  },
   props: ['contact', 'onDelete', 'onEdit', 'onRedirect', 'index'],
 }
 </script>
