b06cc010fe637479532b53b198baa42bb5880483
Ethan <ethanhilljay@outlook.com>
2021-05-13 16:29:59 +0100

    Created store options for data management

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,5 +1,6 @@
 export const state = () => ({
   contacts: [],
+  errors: null,
 })
 
 export const mutations = {
@@ -12,14 +13,33 @@ export const actions = {
   setContacts({ commit }, payload) {
     commit('SET_CONTACTS', payload)
   },
+
   async getContacts({ commit }) {
+    const contacts = await this.$axios.$get(`/contacts/`)
+    commit('SET_CONTACTS', contacts)
+  },
+
+  async createContact({ commit }, { formData, config }) {
+    await this.$axios.$post(`/contacts/`, formData, config)
+    const contacts = await this.$axios.$get(`/contacts/`)
+    commit('SET_CONTACTS', contacts)
+  },
+
+  async deleteContact({ commit }, contactId) {
+    await this.$axios.$delete(`/contacts/${contactId}/`)
+    const contacts = await this.$axios.$get(`/contacts/`)
+    commit('SET_CONTACTS', contacts)
+  },
+
+  async updateContact({ commit }, { id, formData, config }) {
+    await this.$axios.$patch(`/contacts/${id}/`, formData, config)
     const contacts = await this.$axios.$get(`/contacts/`)
     commit('SET_CONTACTS', contacts)
   },
 }
 
-export const getters = () => ({
+export const getters = {
   contacts: (state) => {
     return state.contacts
   },
-})
+}
